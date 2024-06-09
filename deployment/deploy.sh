8cea0a5c2c916e23f899d2e03672fa56182c2a88
Saravana <vsk62.gate@gmail.com>
2024-06-09 14:05:06 +0530

    Uncomment line on deployer script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,7 +1,7 @@
 # Run from the root dir 
 
 set -xe
-# ./deployment/build-blog-resume.sh
+./deployment/build-blog-resume.sh
 cd deployment 
 ansible-playbook -vv deploy.yml -i hosts.yml \
     --private-key ~/.ssh/digital_ocean
